9cce1a205552f4a8022a44add262cc2be364134f
Peter <peter.is.brainy@gmail.com>
2014-07-29 11:30:41 +1200

    added general structure

---
+++ Equilibirum/Equilibirum/GameObjects/GameObject.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Shapes;
+
+namespace Equilibirum.GameObjects
+{
+    interface GameObject
+    {
+        public GameObject()
+        {
+
+        }
+
+        public void draw();
+
+        public Rectangle boundingBox();
+
+        public string getName();
+
+
+    }
+}
---
+++ Equilibirum/Equilibirum/GameObjects/House.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Equilibirum.GameObjects
+{
+    class House : GameObject
+    {
+        private Point coord;
+        private string name;
+
+        public House(Point coord, string name)
+        {
+            this.coord = coord;
+            this.name = name;
+        }
+
+        public void draw()
+        {
+            throw new NotImplementedException();
+        }
+
+        public System.Windows.Shapes.Rectangle boundingBox()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string getName()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
