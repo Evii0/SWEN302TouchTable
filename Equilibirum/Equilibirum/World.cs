ecd96f8991a951391ef0c6155fe4f7104a2c601e
dave <dave.wesson@clear.net.nz>
2014-07-29 13:31:36 +1200

    Added addtional details to Terrain and Board packages

--- Equilibirum/Equilibirum/World.cs
+++ Equilibirum/Equilibirum/World.cs
@@ -1,12 +1,33 @@
-﻿using System;
+﻿using Equilibirum.GameObjects;
+using Equilibirum.Board;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Equilibirum
 {
     class World
     {
+        public Terrain terrain { get; set; }
+        public GameObject[,] gameObjects{get;set;}
+
+        public World()
+        {
+            terrain = new Terrain();
+            gameObjects = new GameObject[100, 100];
+        }
+
+        public void draw()
+        {
+            //THINGS TO DRAW GO HERE
+            throw new NotImplementedException();
+        }
+
+
+
+
     }
 }
